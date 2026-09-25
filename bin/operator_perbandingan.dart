dd6ac0ef2893bca5fc7ad11f0d5bf988666bf4db
zL6663 <rizalaja2835@gmai.com>
2026-09-25 18:12:42 +0800

    ...

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(10 == 10);
+  print(10 != 5);
+  print(10 > 5);
+  print(10 < 5);
+  print(10 >= 10);
+  print(10 <= 5);
+
+  print('Nama' == 'Abu');
+  print('Nama' != 'Rizal');
+}
